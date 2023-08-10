653810fa46943a6cc113f4f9b94551cf2bad0dfa
Willis Plummer <willisplummer@gmail.com>
2023-08-10 09:08:05 -0600

    Initial Commit

---
+++ tetris/tetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  tetrisApp.swift
+//  tetris
+//
+//  Created by Willis Plummer on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
